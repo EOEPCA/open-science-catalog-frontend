30501b6ec5a29a8712bed85fc2e8b7ce1a4e6edc
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2022-01-18 14:41:50 +0200

    add: populate home page with themes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12">
-      <h1>Welcome to the Open Science Catalog</h1>
+  <v-row
+    justify="center"
+    align="center"
+    style="margin: 1px"
+  >
+    <v-col v-for="theme in themes" :key="theme.name" cols="6">
+      <v-img :src="`/img/EO_${theme.name.replace(/\s/g, '')}.jpeg`">
+        <h1>{{ theme.name }}</h1>
+      </v-img>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      themes: [
+        {
+          name: 'Atmosphere'
+        },
+        {
+          name: 'Cryosphere'
+        },
+        {
+          name: 'Land'
+        },
+        {
+          name: 'Magnetic ionosphere'
+        },
+        {
+          name: 'Oceans'
+        },
+        {
+          name: 'Solid earth'
+        }
+      ]
+    }
+  }
 }
 </script>
