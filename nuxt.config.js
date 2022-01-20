45e3d94997514a43c9ae205cd063826514131246
Radu Pașparugă <radupasparuga25@gmail.com>
2022-01-20 16:49:47 +0200

    chore: add meta tags (#9)

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { property: 'og:title', content: 'ESA Open Science Data' },
+      { hid: 'description', name: 'description', content: 'The European Space Agency portal features the latest news in space exploration, human spaceflight, launchers, telecommunications, navigation, monitoring and space science. ' },
+      { property: 'og:description', content: 'The European Space Agency portal features the latest news in space exploration, human spaceflight, launchers, telecommunications, navigation, monitoring and space science.' },
+      { name: 'keywords', content: 'European Space Agency, Moon, Mars, sun, space exploration, probes, missions, satellites, launchers, international space station, ISS, telecommunications, earth observation, navigation, astronauts, solar system, universe, remote sensing, space news, ESA, environment, monitoring, astronomy, Ariane, rocket, Galileo, comet, education, Venus ' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
