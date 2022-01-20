d21fa7c6305993e2acb8869c21ad327462b6ec92
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2022-01-20 18:18:00 +0200

    fix: update store to use better nuxt setup

--- store/index.js
+++ store/index.js
@@ -1,15 +1,7 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-const store = () => new Vuex.Store({
-  state: {
-    packageVersion: process.env.PACKAGE_VERSION || '0'
-  },
-  getters: {
-    appVersion: state => state.packageVersion
-  }
+export const state = () => ({
+  packageVersion: process.env.PACKAGE_VERSION || '0'
 })
 
-export default store
+export const getters = {
+  appVersion: state => state.packageVersion
+}
