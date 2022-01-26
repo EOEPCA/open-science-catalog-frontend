8d1735c990a407fc2d34f309bf968b70a3eb781a
radupasparuga <radupasparuga25@gmail.com>
2022-01-27 01:10:09 +0200

    feat: initial structure of metrics table

---
+++ components/MetricsTable.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="tableWrap">
+    <table role="table" class="tableWrap">
+      <thead class="header">
+        <tr role="row">
+          <th class="expandCell">
+            <v-icon>mdi-chevron-down</v-icon>
+          </th>
+          <th class="nameCell">
+            <v-icon>mdi-chart-bar</v-icon>
+          </th>
+          <th
+            v-for="header in headers"
+            :key="header"
+          >
+            {{ header }}
+          </th>
+          <td class="coverageCell">
+            Coverage
+          </td>
+        </tr>
+      </thead>
+      <tbody role="rowgroup" class="body">
+        <tr v-for="item in items" :key="item.id">
+          <td class="expandCell">
+            <v-icon>mdi-chevron-down</v-icon>
+          </td>
+          <td class="nameCell">
+            {{ item.name }}
+          </td>
+          <td v-for="year in headers" :key="`${item.id}-${year}`">
+            <v-progress-linear
+              v-if="item.years.includes(year)"
+              color="primary"
+              height="25"
+              buffer-value="100"
+            />
+          </td>
+          <td class="coverageCell">
+            <v-icon>mdi-earth</v-icon>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MetricsTable',
+  props: {
+    headers: [],
+    items: []
+  }
+}
+</script>
+
+<style>
+.tableWrap {
+  display: block;
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: auto;
+  max-height: calc(100vh - 180px);
+  font-size: 14px;
+  font-weight: 700;
+  background: white;
+}
+
+.tableWrap table {
+  border-spacing: 0 1px;
+  text-align: center;
+}
+
+.tableWrap table td {
+  border-bottom: 1px solid rgb(204, 204, 204);
+}
+
+.tableWrap table thead th {
+  padding: 10px 10px;
+  border-bottom: 1px solid rgb(204, 204, 204);
+  top: 0;
+  position: sticky;
+  background: white;
+}
+
+.expandCell {
+  min-width: 136px;
+  max-width: 136px;
+  left: 0;
+  position: sticky;
+  background: white;
+  padding: 10px 20px !important;
+  z-index: 100;
+}
+
+.nameCell {
+  min-width: 136px;
+  max-width: 136px;
+  left: 136px;
+  position: sticky;
+  background: white;
+  padding: 10px 20px !important;
+  z-index: 100;
+  border-right: 1px solid rgb(204, 204, 204);
+}
+
+.coverageCell {
+  right: 0;
+  position: sticky;
+  background: white;
+  padding: 10px 20px !important;
+  z-index: 100;
+  border-left: 1px solid rgb(204, 204, 204);
+}
+</style>
--- pages/metrics.vue
+++ pages/metrics.vue
@@ -1,12 +1,209 @@
 <template>
   <v-container>
     <h1>Metrics page</h1>
+    <MetricsTable
+      :headers="metricsHeaders"
+      :items="metricsItems"
+    />
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'Metrics',
+  data () {
+    return {
+      metricsHeaders: [
+        1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996,
+        1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+        2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
+        2021, 2022
+      ],
+      metricsItems: [
+        {
+          id: 'above_ground_biomass',
+          name: 'Above-ground biomass',
+          years: [
+            2018
+          ],
+          numberOfRecords: 1
+        },
+        {
+          id: 'aerosols',
+          name: 'Aerosols',
+          years: [
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+          ],
+          numberOfRecords: 15
+        },
+        {
+          id: 'albedo',
+          name: 'Albedo',
+          years: [
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+          ],
+          numberOfRecords: 6
+        },
+        {
+          id: 'bathymetry',
+          name: 'Bathymetry',
+          years: [
+            2012,
+            2013,
+            2014
+          ],
+          numberOfRecords: 2
+        },
+        {
+          id: 'biomass',
+          name: 'Biomass',
+          years: [
+            2017
+          ],
+          numberOfRecords: 1
+        },
+        {
+          id: 'carbon_flux',
+          name: 'Carbon Flux',
+          years: [
+            2003,
+            2004,
+            2005,
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+          ],
+          numberOfRecords: 1
+        },
+        {
+          id: 'clouds',
+          name: 'Clouds',
+          years: [
+            2006,
+            2007,
+            2008,
+            2009,
+            2010,
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021
+          ],
+          numberOfRecords: 6
+        },
+        {
+          "id": "fresh_water_fluxes",
+          "name": "Fresh water fluxes",
+          "years": [2014, 2015, 2016, 2017],
+          "numberOfRecords": 1
+        },
+        {
+          "id": "geomagnetic_field",
+          "name": "Geomagnetic Field",
+          "years": [
+            2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+            2020, 2021
+          ],
+          "numberOfRecords": 15
+        },
+        {
+          "id": "geophysical_correction_regional_tides",
+          "name": "Geophysical Correction - regional tides",
+          "years": [2014],
+          "numberOfRecords": 1
+        },
+        {
+          "id": "geophysical_correction_wet_troposphere",
+          "name": "Geophysical Correction - wet troposphere",
+          "years": [
+            2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021
+          ],
+          "numberOfRecords": 1
+        },
+        {
+          "id": "glaciers",
+          "name": "Glaciers",
+          "years": [2010, 2011, 2012, 2013, 2014, 2015, 2016],
+          "numberOfRecords": 5
+        },
+        {
+          "id": "ice_sheets_and_ice_shelves",
+          "name": "Ice sheets and ice shelves",
+          "years": [
+            2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021
+          ],
+          "numberOfRecords": 30
+        },
+        {
+          "id": "inorganic_carbon",
+          "name": "Inorganic carbon",
+          "years": [
+            1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996,
+            1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+            2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+          ],
+          "numberOfRecords": 2
+        },
+        {
+          "id": "ionospheric_plasma_density",
+          "name": "Ionospheric plasma density",
+          "years": [
+            2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
+            2014, 2015, 2016, 2017, 2018, 2019, 2020
+          ],
+          "numberOfRecords": 1
+        },
+        {
+          "id": "lakes",
+          "name": "Lakes",
+          "years": [
+            2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+            2020, 2021
+          ],
+          "numberOfRecords": 1
+        },
+      ]
+    }
+  },
   head: {
     title: 'Metrics'
   }
