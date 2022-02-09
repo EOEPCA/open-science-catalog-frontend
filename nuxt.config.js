e214eb7b8c1993cf1d7dffa3e12544478764d50f
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2022-02-09 21:36:28 +0200

    feat: add compression to build files

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,8 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    'nuxt-compress'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
