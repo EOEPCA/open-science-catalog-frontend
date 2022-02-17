c94c0438358e8efb8e224f72a0d282e9a6c45af5
radupasparuga <radupasparuga25@gmail.com>
2022-02-17 19:34:19 +0200

    feat: update projects + variables selector

--- components/ItemGrid.vue
+++ components/ItemGrid.vue
@@ -8,22 +8,28 @@
       md="4"
       lg="3"
     >
-      <v-card>
+      <v-card outlined>
         <v-card-title>
           <v-chip
             small
             label
-            color="primary"
+            :color="type === 'variables' ? 'green' : 'primary'"
+            :dark="type === 'variables' ? true : false"
             class="text-uppercase"
           >
-            Record
+            {{ type.toUpperCase() }}
           </v-chip>
         </v-card-title>
         <v-card-title class="text-subtitle-2 text-uppercase">
           {{ item.name }}
         </v-card-title>
         <v-card-text>
-          {{ item.recordsNumber }} Records
+          <span v-if="type === 'variables'">
+            {{ item.recordsNumber }} Records
+          </span>
+          <span v-else>
+            {{ item.description }}
+          </span>
         </v-card-text>
       </v-card>
     </v-col>
@@ -36,6 +42,10 @@ export default {
     items: {
       type: Array,
       default: () => []
+    },
+    type: {
+      type: String,
+      default: () => ''
     }
   }
 }
