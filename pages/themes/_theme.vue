38dab3ac5af75f5267b20bf1756ad8b913d854d2
radupasparuga <radupasparuga25@gmail.com>
2022-02-17 16:18:28 +0200

    feat: update theme page

--- pages/themes/_theme.vue
+++ pages/themes/_theme.vue
@@ -1,13 +1,103 @@
 <template>
-  <v-container>
-    <h2 class="text-h2 mt-3 mb-5 text-capitalize">
-      {{ theme.name }}
-    </h2>
-    <p>
-      {{ theme.description }}
-    </p>
-    <item-grid :items="items" />
-  </v-container>
+  <div>
+    <v-img
+      :src="withBase(theme.image.replace('/static/', '/')).slice(0, -3) + 'webp'"
+    >
+      <v-breadcrumbs
+        class="ml-5 pt-0 navigationBreadcrumb"
+        :items="navigationBreadcrumb"
+      >
+        <template v-slot:item="{ item }">
+          <v-breadcrumbs-item
+            :href="item.href"
+            :disabled="item.disabled"
+          >
+            <v-chip
+              v-if="navigationBreadcrumb.indexOf(item) === navigationBreadcrumb.length - 1"
+              color="rgb(124, 69, 86)"
+              dark
+              small
+              class="mr-2"
+            >
+              THEME
+            </v-chip>
+            {{ item.text }}
+          </v-breadcrumbs-item>
+        </template>
+      </v-breadcrumbs>
+      <v-container>
+        <v-row class="ml-2">
+          <span class="mt-3 mb-5 themeTitle">
+            {{ theme.name }}
+          </span>
+        </v-row>
+        <v-row class="ml-2">
+          <span class="themeDescription">
+            {{ theme.description }}
+          </span>
+        </v-row>
+        <v-row class="ml-2 mt-6">
+          <span class="themeDescription">
+            <a :href="theme.website">
+              EO4SOCIETY
+            </a>
+          </span>
+        </v-row>
+      </v-container>
+    </v-img>
+    <v-tabs
+      v-model="tab"
+      background-color="#003247"
+      dark>
+      <v-tab>
+        Projects
+      </v-tab>
+      <v-tab>
+        Variables
+      </v-tab>
+    </v-tabs>
+    <v-container>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-row class="pa-6">
+            <span class="text-h2">
+              Projects
+            </span>
+            <v-spacer />
+            <v-text-field
+              v-model="projectsSearch"
+              hide-details
+              solo
+              outlined
+              single-line
+              style="width: 400px !important; flex-grow: 0"
+              label="Search projects"
+              prepend-inner-icon="mdi-magnify"
+            />
+          </v-row>
+          <item-grid :items="items" />
+        </v-tab-item>
+        <v-tab-item>
+          <v-row class="pa-6">
+            <span class="text-h2">
+              Variables
+            </span>
+            <v-spacer />
+            <v-text-field
+              v-model="variablesSearch"
+              hide-details
+              solo
+              outlined
+              single-line
+              style="width: 400px !important; flex-grow: 0"
+              label="Search variables"
+              prepend-inner-icon="mdi-magnify"
+            />
+          </v-row>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -45,6 +135,16 @@ export default {
           name: 'Carbon dioxide, methane and other greenhouse gases',
           recordsNumber: '0'
         }
+      ],
+      tab: 0,
+      variablesSearch: '',
+      projectsSearch: '',
+      navigationBreadcrumb: [
+        {
+          text: 'ESA EO Catalogue',
+          disabled: false,
+          href: '/'
+        }
       ]
     }
   },
@@ -52,6 +152,47 @@ export default {
     return {
       title: this.$route.params.theme.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
     }
+  },
+  created () {
+    this.navigationBreadcrumb.push({
+      text: this.theme.name,
+      disabled: false,
+      href: `/themes/${this.theme.name.toLowerCase()}`
+    })
   }
 }
 </script>
+
+<style>
+.themeTitle {
+  background: #003247;
+  color: white;
+  padding: 1px 10px;
+  position: relative;
+  text-transform: uppercase !important;
+  font-weight: bold !important;
+  font-size: 30px;
+  width: auto !important;
+}
+
+.themeDescription {
+  position: relative;
+  background: rgba(0, 49, 72, 0.733);
+  padding: 10px;
+  color: white;
+  overflow-y: auto;
+}
+
+.themeDescription a {
+  color: white;
+}
+
+.navigationBreadcrumb {
+  background: rgba(255, 255, 255, 0.667);
+  padding: 3px;
+}
+
+.navigationBreadcrumb li {
+  font-size: 17px !important;
+}
+</style>
