d6040e3a641a981d2beb4e5f5676eb6a57a86ff6
radupasparuga <radupasparuga25@gmail.com>
2022-03-01 21:00:43 +0200

    feat: update metainfo display in projects page

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -11,20 +11,78 @@
         <v-row class="ml-2">
           <span class="mt-3 mb-5 text-h2">
             {{ project.properties.title }}
+            <v-chip
+              color="green"
+              dark
+              label
+            >
+              {{ project.properties['osc:status'] }}
+            </v-chip>
           </span>
         </v-row>
-        <v-row class="ml-2 pt-0 mt-0 mb-4">
-          <v-chip
-            color="rgb(124, 69, 86)"
-            dark
-          >
-            {{ project.collection }}
-          </v-chip>
-        </v-row>
-        <v-row class="ml-2">
-          <span>
-            {{ project.properties.description }}
-          </span>
+        <v-row>
+          <v-col>
+            <v-row class="ml-2 pt-0 mt-0 mb-4">
+              <v-chip
+                v-for="theme in project.properties['osc:themes']"
+                :key="theme"
+                class="mx-1"
+                color="rgb(124, 69, 86)"
+                dark
+                label
+              >
+                {{ theme }}
+              </v-chip>
+            </v-row>
+            <v-row class="ml-2 pt-0 mt-0 mb-4">
+              <v-col>
+                <v-icon>
+                  mdi-calendar-today
+                </v-icon>
+                Start date - {{ project.properties.start_datetime.slice(0, -9) }}
+              </v-col>
+              <v-col>
+                <v-icon>
+                  mdi-calendar
+                </v-icon>
+                Estimated end date - {{ project.properties.end_datetime.slice(0, -9) }}
+              </v-col>
+            </v-row>
+            <v-row class="ml-2 pt-0 mt-0 mb-4 text-h6">
+              <v-icon>
+                mdi-account-multiple
+              </v-icon>
+              Consortium
+            </v-row>
+            <v-row class="ml-2 pt-0 mt-0 mb-4">
+              {{ project.properties['osc:consortium'] }}
+            </v-row>
+            <v-row class="ml-2 pt-0 mt-0 mb-4">
+              <a
+                v-for="link in project.links.filter(el => el.rel === 'via')"
+                :key="link"
+                :href="link.href"
+                target="_blank"
+                class="mx-1 projectLink"
+              >
+                <v-icon>
+                  mdi-link
+                </v-icon>
+                WEBSITE
+              </a>
+            </v-row>
+          </v-col>
+          <v-col>
+            <v-row class="ml-2 text-h6">
+              <v-icon>
+                mdi-file-document
+              </v-icon>
+              Description
+            </v-row>
+            <v-row class="ml-2">
+              {{ project.properties.description }}
+            </v-row>
+          </v-col>
         </v-row>
       </v-container>
     </div>
@@ -111,5 +169,8 @@ export default {
 <style>
 .variableHeaderContainer {
   border-bottom: 0.25em solid #335E6F;
+}
+.projectLink {
+  text-decoration: none;
 }
 </style>
