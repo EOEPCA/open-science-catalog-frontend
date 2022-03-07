a2c4c2cd0c5dc774a9e3a229ddbc85971fd2bb35
silvester-pari <silvester.pari@eox.at>
2022-03-07 18:14:02 +0100

    feat: add f=json to all catalog links

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,3 +1,15 @@
+const getUriWithParam = (baseUrl, params) => {
+  const Url = new URL(baseUrl)
+  const urlParams = new URLSearchParams(Url.search)
+  for (const key in params) {
+    if (params[key] !== undefined) {
+      urlParams.set(key, params[key])
+    }
+  }
+  Url.search = urlParams.toString()
+  return Url.toString()
+}
+
 export default function ({ $axios }, inject) {
   const metadataApi = $axios.create()
   metadataApi.setBaseURL('https://eoepca.github.io/open-science-catalog-metadata')
@@ -13,5 +25,13 @@ export default function ({ $axios }, inject) {
 
   const catalogApi = $axios.create()
   catalogApi.setBaseURL('https://resource-catalogue.osc.develop.eoepca.org')
+  // Add "f=json" query param to all requests going to the dynamic catalog
+  catalogApi.onRequest((config) => {
+    const newUrl = getUriWithParam(config.baseURL + config.url, { f: 'json' })
+    return {
+      ...config,
+      url: newUrl
+    }
+  })
   inject('catalogApi', catalogApi)
 }
