a2480445bcd040e3ff72d35cf77b4a35f4603288
silvester-pari <silvester.pari@eox.at>
2022-03-21 12:01:11 +0100

    chore: lint

--- plugins/slugify.js
+++ plugins/slugify.js
@@ -4,12 +4,12 @@ const mixin = {
   methods: {
     slugify (text) {
       return text.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/_/g, '-')           // Replace underscores _ with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/_/g, '-') // Replace underscores _ with -
+        .replace(/[^\w-]+/g, '') // Remove all non-word chars
+        .replace(/--+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, '') // Trim - from end of text
     }
   }
 }
