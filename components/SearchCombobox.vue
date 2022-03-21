e97b2decded4203bc3d845b133433cfd8b4c41bb
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2022-03-21 11:51:18 +0200

    chore: separate search combobox in separate component

---
+++ components/SearchCombobox.vue
@@ -0,0 +1,298 @@
+<template>
+  <div
+    class="d-flex align-center"
+  >
+    <div
+      class="
+          d-flex
+          lightgrey
+          align-center"
+      style="width: 100%; position: relative"
+    >
+      <div style="position: absolute; width: 100%">
+        <v-text-field
+          style="pointer-events: none;"
+          label="Filter items"
+          :value="mainInputValue"
+          :class="mainInputValue ? 'v-input--is-focused' : ''"
+          height="42"
+        />
+      </div>
+      <v-chip
+        v-for="(item, index) in filterItems"
+        :key="index"
+        small
+        :close="!!item.value"
+        class="mr-1"
+        @click:close="remove(item)"
+      >
+        <span class="text-capitalize">
+          {{ item.key }}
+        </span>
+        <span
+          v-if="
+            !!item.value || item.operator"
+          class="px-1"
+        >
+          {{ item.operator || ':' }}</span>
+        <strong>{{ !!item.value ? ` ${item.value}` : '' }}</strong>
+      </v-chip>
+      <v-combobox
+        v-if="currentlyFreeText"
+        ref="headless"
+        placeholder="..."
+        label=" "
+        :items="[
+        ]"
+        chips
+        :search-input.sync="searchText"
+        class="headless-input"
+        :type="isNumberField ? 'number' : 'text'"
+        @change="select"
+        @focus="mainInputValue = ' '"
+        @keydown.delete="onDelete"
+      >
+        <template #no-data>
+          <v-list-item @click="select(searchText)">
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ searchText }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-combobox>
+      <v-autocomplete
+        v-else
+        ref="headless"
+        v-model="filterModel"
+        :items="
+          filterFields"
+        chips
+        item-text="field_name"
+        return-object
+        label=" "
+        class="headless-input"
+        @input="select"
+        @focus="mainInputValue = ' '"
+        @keyup.enter="onEnter"
+        @keyup.delete="onDelete"
+      >
+        <template #item="data">
+          <span :class="data.item.operator ? '' : 'text-capitalize'">
+            {{ data.item.field_name }}
+          </span>
+        </template>
+      </v-autocomplete>
+    </div>
+    <v-btn
+      color="primary"
+      class="ml-3"
+      :disabled="filterItems.length < 1 || !!filterItems.find(f => f.value === null)"
+      :loading="loading"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchCombobox',
+  data () {
+    return {
+      filterModel: null,
+      filterItems: [
+      ],
+      mainInputValue: null,
+      inputComplete: false,
+      searchText: null,
+      loading: false
+    }
+  },
+  computed: {
+    currentlyFreeText () {
+      return this.filterItems && this.filterItems.find((i) => {
+        return !i.value && i.operator
+      })
+    },
+    isNumberField () {
+      // const currentFilter = this.filterItems.find(f => f.value === null)
+      // const currentField = this.metaInfo.fields.find(f => f.field_name === currentFilter.key)
+      return false
+    },
+    filterFields () {
+      let items
+      const allItems = [
+        {
+          field_name: 'theme',
+          filter: 'like'
+        },
+        {
+          field_name: 'variable',
+          filter: 'like'
+        },
+        {
+          field_name: 'project',
+          filter: 'like'
+        },
+        {
+          field_name: 'product',
+          filter: 'like'
+        },
+        {
+          field_name: 'startDate',
+          filter: 'range',
+          digits: 0
+        }
+      ]
+      const inProgressItem = this.filterItems.find(f => f.value === null)
+      if (inProgressItem || this.currentlyFreeText) {
+        const currentMeta = inProgressItem &&
+          allItems.find(f => f.field_name === inProgressItem.key)
+        if (inProgressItem.operator) {
+          items = [
+          ]
+        } else if (currentMeta.filter === 'boolean') {
+          items = [
+            {
+              filter_value: true,
+              field_name: 'true',
+              original_field_name: inProgressItem.key
+            },
+            {
+              filter_value: false,
+              field_name: 'false',
+              original_field_name: inProgressItem.key
+            }
+          ]
+        } else if (currentMeta.filter === 'exact') {
+          items = [
+            {
+              filter_value: null,
+              field_name: 'is exactly',
+              operator: '=',
+              original_field_name: inProgressItem.key
+            }
+          ]
+        } else if (currentMeta.filter === 'like') {
+          items = [
+            {
+              filter_value: null,
+              field_name: 'includes',
+              operator: 'includes',
+              original_field_name: inProgressItem.key
+            }
+          ]
+        } else if (currentMeta.filter === 'range') {
+          const existingOperator = this.filterItems
+            .find(i => i.key === inProgressItem.key && !!i.operator)
+          items = [
+            ...((existingOperator ? existingOperator.operator !== '≤' : true)
+              ? [
+                  {
+                    filter_value: null,
+                    field_name: '≤ lower than or equal}',
+                    operator: '≤',
+                    original_field_name: inProgressItem.key
+                  }
+                ]
+              : [
+                ]),
+            ...((existingOperator ? existingOperator.operator !== '≥' : true)
+              ? [
+                  {
+                    filter_value: null,
+                    field_name: '≥ higher than or equal',
+                    operator: '≥',
+                    original_field_name: inProgressItem.key
+                  }
+                ]
+              : [
+                ])
+          ]
+        } else {
+          items = allItems
+        }
+      } else {
+        items = allItems
+      }
+      return items
+    }
+  },
+  methods: {
+    select (item) {
+      if (item) {
+        if (this.currentlyFreeText) {
+          this.filterItems = this.filterItems
+            .map((i) => {
+              if (!i.value && i.operator) {
+                i.value = item
+              }
+              return i
+            })
+        } else if (item.operator) {
+          this.filterItems = this.filterItems
+            .map((i) => {
+              if (!i.value) {
+                i.operator = item.operator
+              }
+              return i
+            })
+        } else if (item.filter_value) {
+          this.filterItems = this.filterItems
+            .map((i) => {
+              if (!i.value) {
+                i.value = item.filter_value
+              }
+              return i
+            })
+        } else {
+          this.filterItems.push({
+            key: item.field_name,
+            value: null
+          })
+        }
+        this.inputComplete = false
+        setTimeout(() => { this.inputComplete = true }, 300)
+        this.$nextTick(() => {
+          this.$refs.headless.focus()
+          if (!this.currentlyFreeText) {
+            this.$refs.headless.activateMenu()
+          }
+        })
+      }
+    },
+    remove (item) {
+      this.filterItems.splice(this.filterItems.indexOf(item), 1)
+    },
+    onEnter () {
+      if (this.inputComplete && this.filterItems.every(i => !!i.value)) {
+        this.submit()
+      }
+    },
+    onDelete () {
+      if (this.filterItems.length < 1) {
+        return
+      }
+      if (this.searchText) {
+        return
+      }
+      this.filterItems.pop()
+      this.filterModel = null
+    },
+    submit () {
+      const searchQuery = this.filterItems.reduce((acc, curr) => `${acc}&${
+        curr.operator
+          ? 'q'
+            // .replace('≤', '__lte') todo: range if needed
+            // .replace('≥', '__gte')
+            // .replace('=', '')
+          : curr.key
+      }=${curr.value}`, '')
+      this.$emit('searchQuery', searchQuery)
+    }
+  }
+}
+</script>
