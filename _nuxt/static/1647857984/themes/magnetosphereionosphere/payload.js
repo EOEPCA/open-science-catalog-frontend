a270bb41d9fd388b704d758822df50f9ed3b7d47
silvester-pari <silvester-pari@users.noreply.github.com>
2022-03-21 11:04:54 +0000

    deploy: 4d531a25f245e1ea449df3dcb461b047704ef639

--- _nuxt/static/1647857984/themes/magnetosphereionosphere/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/themes/magnetosphereionosphere", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1647860688/themes/magnetosphere-ionosphere/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/themes/magnetosphere-ionosphere", {data:[{}],fetch:{},mutations:[]});
