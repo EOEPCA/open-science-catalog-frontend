13c93f32c49615eddd35ba9cd8457afdb72cbebd
silvester-pari <silvester.pari@eox.at>
2022-04-29 12:14:37 +0200

    fix: render authentication-depending components only only on client side

--- layouts/default.vue
+++ layouts/default.vue
@@ -42,59 +42,61 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-group
-          v-if="$auth.loggedIn"
-          prepend-icon="mdi-plus-circle-outline"
-          no-action
-        >
-          <template #activator>
-            <v-list-item-content>
-              <v-list-item-title>
-                Contribute
-              </v-list-item-title>
-            </v-list-item-content>
-          </template>
+        <client-only>
+          <v-list-group
+            v-if="$auth.loggedIn"
+            prepend-icon="mdi-plus-circle-outline"
+            no-action
+          >
+            <template #activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Contribute
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
 
-          <v-list-item
-            to="/add-item"
-            router
-            exact
-            class="pl-10 primary"
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-file-plus-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Add item
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item
-            to="/edit-item"
-            router
-            exact
-            class="pl-10 primary"
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-file-edit-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Edit item
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item
-            to="/contribution-status"
-            router
-            exact
-            class="pl-10 primary"
-          >
-            <v-list-item-icon>
-              <v-icon>mdi-file-clock-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Status
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-group>
+            <v-list-item
+              to="/add-item"
+              router
+              exact
+              class="pl-10 primary"
+            >
+              <v-list-item-icon>
+                <v-icon>mdi-file-plus-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>
+                Add item
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+              to="/edit-item"
+              router
+              exact
+              class="pl-10 primary"
+            >
+              <v-list-item-icon>
+                <v-icon>mdi-file-edit-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>
+                Edit item
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+              to="/contribution-status"
+              router
+              exact
+              class="pl-10 primary"
+            >
+              <v-list-item-icon>
+                <v-icon>mdi-file-clock-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>
+                Status
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-group>
+        </client-only>
 
         <v-list-item
           to="/metrics"
@@ -144,34 +146,36 @@
         </v-list-item>
       </v-list>
       <template #append>
-        <div class="pa-2">
-          <template
-            v-if="$auth.loggedIn"
-          >
-            <p class="white--text text-center mb-0">
+        <client-only>
+          <div class="pa-2">
+            <template
+              v-if="$auth.loggedIn"
+            >
+              <p class="white--text text-center mb-0">
               Hello, {{ $auth.user.given_name }} {{ $auth.user.family_name }} {{ $auth.user.name }}
-            </p>
-            <p class="white--text text-center">
-              <small>OSCDataOwner: {{ $auth.user.OSCDataOwner }}</small>
-              <v-icon
-                small
-                :color="$auth.user.OSCDataOwner ? 'success' : 'error'"
-              >
-                {{ $auth.user.OSCDataOwner ? 'mdi-check-decagram' : 'mdi-close-circle-outline' }}
+              </p>
+              <p class="white--text text-center">
+                <small>OSCDataOwner: {{ $auth.user.OSCDataOwner }}</small>
+                <v-icon
+                  small
+                  :color="$auth.user.OSCDataOwner ? 'success' : 'error'"
+                >
+                  {{ $auth.user.OSCDataOwner ? 'mdi-check-decagram' : 'mdi-close-circle-outline' }}
+                </v-icon>
+              </p>
+            </template>
+            <v-btn
+              color="secondary"
+              block
+              :to="$auth.loggedIn ? '/logout' : '/login'"
+            >
+              <v-icon left>
+                mdi-account-outline
               </v-icon>
-            </p>
-          </template>
-          <v-btn
-            color="secondary"
-            block
-            :to="$auth.loggedIn ? '/logout' : '/login'"
-          >
-            <v-icon left>
-              mdi-account-outline
-            </v-icon>
-            {{ $auth.loggedIn ? 'Log out' : 'Log in' }}
-          </v-btn>
-        </div>
+              {{ $auth.loggedIn ? 'Log out' : 'Log in' }}
+            </v-btn>
+          </div>
+        </client-only>
       </template>
     </v-navigation-drawer>
     <v-app-bar
