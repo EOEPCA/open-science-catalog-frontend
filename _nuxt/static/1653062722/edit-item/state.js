91064e82b100bd003636478397ef6d2deea98c33
silvester-pari <silvester-pari@users.noreply.github.com>
2022-05-20 16:10:23 +0000

    deploy: 1aef777b2e56f9391f678a2d2f363fccebe13f5c

--- _nuxt/static/1653062722/edit-item/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fopen-science-catalog-frontend\u002F_nuxt\u002Fstatic\u002F1653062722",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fedit-item",config:{_app:{basePath:"\u002Fopen-science-catalog-frontend\u002F",assetsPath:"\u002Fopen-science-catalog-frontend\u002F_nuxt\u002F",cdnURL:a}}}}(null));
