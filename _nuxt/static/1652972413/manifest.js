baaf506cbb88ec81d18a538ee1670a4ed8b941d9
silvester-pari <silvester-pari@users.noreply.github.com>
2022-05-20 07:11:25 +0000

    deploy: 053e90f71723bcf95b2c319029ed2c989c269d9d

--- _nuxt/static/1652972413/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Flogin","\u002Foauth-callback","\u002Fmetrics","\u002Flogout","\u002Fsearch","\u002F","\u002Fthemes\u002Fatmosphere","\u002Fthemes\u002Fcryosphere","\u002Fthemes\u002Fland","\u002Fthemes\u002Fmagnetosphere-ionosphere","\u002Fthemes\u002Fsolid-earth","\u002Fthemes\u002Foceans"]})
---
+++ _nuxt/static/1653030678/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Flogout","\u002Fmetrics","\u002Flogin","\u002Fsearch","\u002Foauth-callback","\u002F","\u002Fthemes\u002Fatmosphere","\u002Fthemes\u002Fcryosphere","\u002Fthemes\u002Fmagnetosphere-ionosphere","\u002Fthemes\u002Foceans","\u002Fthemes\u002Fland","\u002Fthemes\u002Fsolid-earth"]})
