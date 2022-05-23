05630c35a70768e2f7c95db9e48f749ed3b8766a
silvester-pari <silvester.pari@eox.at>
2022-05-23 16:36:56 +0200

    fix: request encoding

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,3 @@
-const getUriWithParam = (baseUrl, params) => {
-  const Url = new URL(baseUrl)
-  const urlParams = new URLSearchParams(Url.search)
-  for (const key in params) {
-    if (params[key] !== undefined) {
-      urlParams.set(key, params[key])
-    }
-  }
-  Url.search = urlParams.toString()
-  return Url.toString()
-}
-
 export default function ({
   $axios,
   $config: { staticEndpoint, staticBaseToReplace, dynamicEndpoint, backendEndpoint }
@@ -31,7 +19,7 @@ export default function ({
   dynamicCatalog.setBaseURL(dynamicEndpoint)
   // Add "f=json" query param to all requests going to the dynamic catalog
   dynamicCatalog.onRequest((config) => {
-    const newUrl = getUriWithParam(config.baseURL + config.url, { f: 'json' })
+    const newUrl = `${config.url}&f=json`
     return {
       ...config,
       url: newUrl
