85c12a429652a7337c0df7549f0b3b55cf4001f9
radupasparuga <radupasparuga25@gmail.com>
2022-06-14 17:48:35 +0300

    feat: update contribution status style

--- pages/contribution-status.vue
+++ pages/contribution-status.vue
@@ -17,87 +17,95 @@
     </v-row>
     <v-row class="d-flex justify-center">
       <v-col cols="12">
-        <v-list
-          subheader
-          two-line
-          rounded
+        <v-data-table
+          :headers="[
+            {
+              text: 'Item type',
+              value: 'item_type'
+            },
+            {
+              text: 'File name',
+              value: 'filename'
+            },
+            {
+              text: 'Status',
+              value: 'state'
+            },
+            {
+              text: 'Change type',
+              value: 'change_type'
+            },
+            {
+              text: 'Pull request',
+              value: 'url'
+            }
+          ]"
+          :items="contributionItems"
+          class="elevation-1"
+          :loading="!loaded"
+          dense
+          :items-per-page="20"
+          :footer-props="{
+            'items-per-page-options': [10, 20, 30, 40, 50]
+          }"
         >
-          <div
-            v-if="!loaded"
-            class="d-flex justify-center pa-5 py-10"
-          >
-            <v-progress-circular
-              indeterminate
-              color="primary"
-              :size="50"
-            />
-          </div>
-          <template
-            v-for="itemType in Object.keys(pendingItems)"
-          >
-            <v-subheader
-              :key="itemType"
-              class="text-capitalize text-h4"
-              style="padding: 0 0 0 0;"
+          <template #item.state="{ item }">
+            <v-chip
+              :color="getStatusStyle(item.state).color"
+              small
+              dark
             >
-              {{ itemType }}
-            </v-subheader>
-            <div
-              v-if="pendingItems[itemType].length === 0"
-              :key="itemType"
-              class="text-h6"
-            >
-              No available items
-            </div>
-            <v-list-item
-              v-for="(item, key) in pendingItems[itemType]"
-              :key="`${itemType}-${key}`"
-              class="pl-0"
-            >
-              <v-list-item-avatar>
-                <v-icon
-                  :class="getStatusStyle(itemType).color"
-                  dark
-                >
-                  {{ getStatusStyle(itemType).icon }}
-                </v-icon>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>
-                  <strong>{{ item.change_type }} request</strong>: {{ item.filename }}
-                </v-list-item-title>
-
-                <v-list-item-subtitle>
-                  <v-btn
-                    dark
-                    x-small
-                    :color="$typeColor(itemType.replace('s', ''))"
-                  >
-                    {{ itemType.replace('s', '') }}
-                  </v-btn>
-                </v-list-item-subtitle>
-              </v-list-item-content>
-
-              <v-list-item-action>
-                <v-btn
-                  color="black"
-                  dark
-                  small
-                  :href="item.url"
-                  target="_blank"
-                >
-                  <v-icon
-                    left
-                  >
-                    mdi-github
-                  </v-icon>
-                  View on GitHub
-                </v-btn>
-              </v-list-item-action>
-            </v-list-item>
+              <v-icon small left>
+                {{ getStatusStyle(item.state).icon }}
+              </v-icon>
+              {{ item.state }}
+            </v-chip>
           </template>
-        </v-list>
+          <template #item.item_type="{ item }">
+            <v-chip
+              :color="$typeColor(item.item_type.toLowerCase())"
+              small
+              label
+              dark
+            >
+              {{ item.item_type }}
+            </v-chip>
+          </template>
+          <template #item.change_type="{ item }">
+            <v-btn
+              color="black"
+              plain
+              small
+              class="px-0"
+            >
+              <v-icon
+                small
+                left
+              >
+                {{ getChangeIcon(item.change_type) }}
+              </v-icon>
+              {{ item.change_type }}
+            </v-btn>
+          </template>
+          <template #item.url="{ item }">
+            <v-btn
+              color="black"
+              plain
+              small
+              :href="item.url"
+              target="_blank"
+              class="px-0"
+            >
+              #{{ item.url.split('/')[item.url.split('/').length - 1] }}
+              <v-icon
+                small
+                right
+              >
+                mdi-open-in-new
+              </v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
       </v-col>
     </v-row>
   </v-container>
@@ -109,34 +117,25 @@ export default {
   name: 'ContributionsStatus',
   middleware: 'auth',
   data: () => ({
-    pendingItems: {},
+    contributionItems: [],
     loaded: false
   }),
   head: {
     title: 'Contributions Status'
   },
   async created () {
-    const themes = await this.$metadataBackend.$get('/item-requests/themes')
-    const variables = await this.$metadataBackend.$get('/item-requests/variables')
-    const projects = await this.$metadataBackend.$get('/item-requests/projects')
-    const products = await this.$metadataBackend.$get('/item-requests/products')
-    this.pendingItems = {
-      Pending: [],
-      Accepted: [],
-      Rejected: []
-    }
-    themes.items.forEach((item) => {
-      this.pendingItems[item.state].push(item)
-    })
-    variables.items.forEach((item) => {
-      this.pendingItems[item.state].push(item)
-    })
-    projects.items.forEach((item) => {
-      this.pendingItems[item.state].push(item)
-    })
-    products.items.forEach((item) => {
-      this.pendingItems[item.state].push(item)
-    })
+    const [themes, variables, projects, products] = await Promise.all([
+      this.$metadataBackend.$get('/item-requests/themes'),
+      this.$metadataBackend.$get('/item-requests/variables'),
+      this.$metadataBackend.$get('/item-requests/projects'),
+      this.$metadataBackend.$get('/item-requests/products')
+    ])
+    this.contributionItems = [
+      ...themes.items.map(item => ({ ...item, item_type: 'Theme' })),
+      ...variables.items.map(item => ({ ...item, item_type: 'Variable' })),
+      ...projects.items.map(item => ({ ...item, item_type: 'Project' })),
+      ...products.items.map(item => ({ ...item, item_type: 'Product' }))
+    ]
     this.loaded = true
   },
   methods: {
@@ -159,6 +158,21 @@ export default {
           styling.icon = 'mdi-progress-close'
       }
       return styling
+    },
+    getChangeIcon (type) {
+      let icon = ''
+      switch (type) {
+        case 'Add':
+          icon = 'mdi-file-plus-outline'
+          break
+        case 'Update':
+          icon = 'mdi-file-edit-outline'
+          break
+        case 'Delete':
+          icon = 'mdi-file-remove-outline'
+          break
+      }
+      return icon
     }
   }
 }
