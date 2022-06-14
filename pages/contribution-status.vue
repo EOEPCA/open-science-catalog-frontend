e7e555f77892ff3c657f584e165787951fe20b1d
radupasparuga <radupasparuga25@gmail.com>
2022-06-14 15:42:26 +0300

    feat: update contribution component to include status

--- pages/contribution-status.vue
+++ pages/contribution-status.vue
@@ -1,8 +1,20 @@
 <template>
   <v-container :class="$vuetify.breakpoint.lgAndUp ? 'px-15 pt-8' : 'pa-4'">
     <h2 class="text-h2 mt-3 mb-5">
-      Pending Contributions
+      Contributions status
     </h2>
+    <v-row class="d-flex justify-center text-h6">
+      <v-col cols="12">
+        <v-icon>
+          mdi-information-outline
+        </v-icon>
+        Note: only your most recent contributions are shown in this list. For a full list of contributions,
+        please refer to the
+        <a href="https://github.com/EOEPCA/open-science-catalog-metadata/pulls" target="_blank" style="text-decoration: none;">
+          Open Science Catalog Metadata GitHub Repository
+        </a>.
+      </v-col>
+    </v-row>
     <v-row class="d-flex justify-center">
       <v-col cols="12">
         <v-list
@@ -21,26 +33,33 @@
             />
           </div>
           <template
-            v-for="itemType in Object.keys(pendingItems).filter(k => pendingItems[k].items.length > 0)"
+            v-for="itemType in Object.keys(pendingItems)"
           >
-            <!-- <v-subheader
+            <v-subheader
               :key="itemType"
-              class="text-capitalize"
+              class="text-capitalize text-h4"
+              style="padding: 0 0 0 0;"
             >
-              {{itemType}}
-            </v-subheader> -->
-
+              {{ itemType }}
+            </v-subheader>
+            <div
+              v-if="pendingItems[itemType].length === 0"
+              :key="itemType"
+              class="text-h6"
+            >
+              No available items
+            </div>
             <v-list-item
-              v-for="(item, key) in pendingItems[itemType].items"
+              v-for="(item, key) in pendingItems[itemType]"
               :key="`${itemType}-${key}`"
               class="pl-0"
             >
               <v-list-item-avatar>
                 <v-icon
-                  class="warning"
+                  :class="getStatusStyle(itemType).color"
                   dark
                 >
-                  mdi-progress-clock
+                  {{ getStatusStyle(itemType).icon }}
                 </v-icon>
               </v-list-item-avatar>
 
@@ -102,12 +121,45 @@ export default {
     const projects = await this.$metadataBackend.$get('/item-requests/projects')
     const products = await this.$metadataBackend.$get('/item-requests/products')
     this.pendingItems = {
-      themes,
-      variables,
-      projects,
-      products
+      Pending: [],
+      Accepted: [],
+      Rejected: []
     }
+    themes.items.forEach((item) => {
+      this.pendingItems[item.state].push(item)
+    })
+    variables.items.forEach((item) => {
+      this.pendingItems[item.state].push(item)
+    })
+    projects.items.forEach((item) => {
+      this.pendingItems[item.state].push(item)
+    })
+    products.items.forEach((item) => {
+      this.pendingItems[item.state].push(item)
+    })
     this.loaded = true
+  },
+  methods: {
+    getStatusStyle (type) {
+      const styling = {
+        color: '',
+        icon: ''
+      }
+      switch (type) {
+        case 'Pending':
+          styling.color = 'warning'
+          styling.icon = 'mdi-progress-clock'
+          break
+        case 'Accepted':
+          styling.color = 'success'
+          styling.icon = 'mdi-progress-check'
+          break
+        case 'Rejected':
+          styling.color = 'error'
+          styling.icon = 'mdi-progress-close'
+      }
+      return styling
+    }
   }
 }
 </script>
