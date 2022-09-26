fedbf78d08616be96f359534f83d2698a6ce8cdc
silvester-pari <silvester-pari@users.noreply.github.com>
2022-09-26 11:38:39 +0000

    deploy: 3ce7a1b8103d03650a8206b35e33162a23919128

--- _nuxt/static/1664190973/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Flogin","\u002Fprivacy-policy","\u002Fterms","\u002Foauth-callback","\u002Flogout","\u002Fmetrics","\u002Fsearch","\u002F"]})
---
+++ _nuxt/static/1664192312/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Flogin","\u002Flogout","\u002Fmetrics","\u002Foauth-callback","\u002Fterms","\u002Fprivacy-policy","\u002Fsearch","\u002F"]})
