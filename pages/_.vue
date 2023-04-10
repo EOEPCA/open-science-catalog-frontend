c9221e7233019eac726be00a5fb7643330d06ceb
radupasparuga <radupasparuga25@gmail.com>
2023-04-10 06:01:33 +0300

    chore: update url structure

--- pages/_.vue
+++ pages/_.vue
@@ -9,25 +9,35 @@
 <script>
 export default {
     mounted() {
-        // const match = ['projects', 'products']
-        // for (const type of match) {
-        //     if (this.$route.path.includes(`/${type}`)) {
-        //         const curr = this.$route.path.replace(`/${type}/`, '')
-        //         if (type === 'products') {
-        //             // actually need to go deeper here
-        //         }
-        //         document.querySelector('iframe').contentWindow.postMessage({
-        //             navigate: true,
-        //             type,
-        //             id: curr
-        //         }, '*')
-        //     }
-        // }
-        // window.addEventListener('message', (evt) => {
-        //     if (evt.data && evt.data.navigate) {
-        //         this.$router.replace(`/${evt.data.type}/${evt.data.id}`)
-        //     }
-        // })
+        window.addEventListener('message', (evt) => {
+            if (evt.data.status) {
+                // const match = ['projects', 'products']
+                // console.log(this.$route.path)
+                // for (const type of match) {
+                //     if (this.$route.path.includes(`/${type}`)) {
+                //         const curr = this.$route.path.replace(`/${type}/`, '')
+                //         if (type === 'products') {
+                //             // actually need to go deeper here
+                //         }
+                //         document.querySelector('iframe').contentWindow.postMessage({
+                //             navigate: true,
+                //             type,
+                //             id: curr
+                //         }, '*')
+                //     }
+                // }
+                if (!this.$route.path.includes('browse')) {
+                    document.querySelector('iframe').contentWindow.postMessage({
+                        navigate: true,
+                        id: this.$route.path
+                    }, '*')
+                }
+            } else if (evt.data && evt.data.navigate) {
+                if (`/${evt.data.id}` !== this.$route.path) {
+                    this.$router.replace(`/${evt.data.id}`)
+                }
+            }
+        })
     }
 }
 </script>
