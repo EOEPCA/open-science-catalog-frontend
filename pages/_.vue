475791b805d616e38191327f6c95eb0cbdaa98ed
silvester-pari <silvester.pari@eox.at>
2023-05-22 06:51:34 +0000

    feat: use local stac-browser bundle in iframe

--- pages/_.vue
+++ pages/_.vue
@@ -1,40 +1,49 @@
 <template>
-    <div class="browse">
-        <iframe
-            :src="iframeSrc"
-            border="none"
-            style="width: 100%; height: 100%"
-        ></iframe>
-        <edit-button />
-    </div>
+  <div class="browse">
+    <iframe
+      :src="iframeSrc"
+      border="none"
+      style="width: 100%; height: 100%"
+    ></iframe>
+    <edit-button />
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        iframeSrc: null,
-        srcBase: 'http://localhost:8080/external/raw.githubusercontent.com/EOEPCA/open-science-catalog-metadata-testing/from-csvs',
-        firstLoadDone: false
-    }),
-    mounted() {
-        this.iframeSrc = `${this.srcBase}${this.$route.path}.json`
-        console.log(this.iframeSrc)
-        window.addEventListener('message', (evt) => {
-            if (evt.data && evt.data.navigate && this.firstLoadDone) {
-                const stringToReplace = '/external/raw.githubusercontent.com/EOEPCA/open-science-catalog-metadata-testing/from-csvs'
-                const parsedPath = evt.data.navigate.replace(stringToReplace, '').replace('.json', '')
-                history.replaceState({}, null, parsedPath)
-            } else {
-                this.firstLoadDone = true
-            }
-        })
-    }
-}
+  data: () => ({
+    iframeSrc: null,
+    firstLoadDone: false,
+  }),
+  mounted() {
+    this.iframeSrc = `/stac-browser/?external=${this.prepareEndpoint(
+      this.$config.staticEndpoint
+    )}${this.$route.path}.json`;
+    window.addEventListener("message", (evt) => {
+      if (evt.data && evt.data.navigate && this.firstLoadDone) {
+        const stringToReplace = `/external/${this.prepareEndpoint(
+          this.$config.staticEndpoint
+        )}`;
+        const parsedPath = evt.data.navigate
+          .replace(stringToReplace, "")
+          .replace(".json", "");
+        history.replaceState({}, null, parsedPath);
+      } else {
+        this.firstLoadDone = true;
+      }
+    });
+  },
+  methods: {
+    prepareEndpoint(src) {
+      return src.replace("https://", "").replace("http://", "");
+    },
+  },
+};
 </script>
 
 <style scoped>
 .browse {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 </style>
