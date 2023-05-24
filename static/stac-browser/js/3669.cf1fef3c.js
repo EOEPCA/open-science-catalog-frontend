d76831b2cc01427d4eac9e0161924414b1ebf42f
silvester-pari <silvester.pari@eox.at>
2023-05-24 05:43:57 +0000

    chore: update stac browser bundle

--- static/stac-browser/js/3669.cf1fef3c.js
+++ static/stac-browser/js/3669.cf1fef3c.js
@@ -1,13 +1,2 @@
-"use strict";
-(self["webpackChunk_radiantearth_stac_browser"] =
-  self["webpackChunk_radiantearth_stac_browser"] || []).push([
-  [3669],
-  {
-    3669: function (a, r, e) {
-      e.r(r);
-      var t = e(8168);
-      r["default"] = t.fr;
-    },
-  },
-]);
+"use strict";(self["webpackChunk_radiantearth_stac_browser"]=self["webpackChunk_radiantearth_stac_browser"]||[]).push([[3669],{3669:function(a,r,e){e.r(r);var t=e(8168);r["default"]=t.fr}}]);
 //# sourceMappingURL=3669.cf1fef3c.js.map
