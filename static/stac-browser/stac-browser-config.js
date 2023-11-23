b6cbe0bef6bd74e307d4bc3124877d09da6610c0
A-Behairi <behairiahmed@gmail.com>
2023-11-23 18:40:28 +0100

    chore: clean up

--- static/stac-browser/stac-browser-config.js
+++ static/stac-browser/stac-browser-config.js
@@ -1,4 +1,4 @@
 window.STAC_BROWSER_CONFIG = {
   //check dockerfile and docker-entry.sh before editing
-  catalogUrl: process.env.STATIC_ENDPOINT
+  catalogUrl: "https://metadata.testing.opensciencedata.esa.int/open-science-catalog-metadata-testing/catalog.json"
 }
