10b10444589820e6ebe1a0d43d697c78d42a5f78
A-Behairi <behairiahmed@gmail.com>
2023-12-07 15:11:41 +0100

    chore: remove nuxt/ts-build + transpile chartjs

--- nuxt.config.js
+++ nuxt.config.js
@@ -117,8 +117,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/vuetify
     "@nuxtjs/vuetify",
   ],
@@ -175,6 +173,7 @@ export default {
       "ol/expr/gpu",
       "ol/style/RegularShape",
       "marked/lib/marked",
+      "chart.js"
     ],
   },
 
