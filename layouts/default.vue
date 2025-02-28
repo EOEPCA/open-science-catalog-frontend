551db52272a1d2920463f90e086d7b19b79ddebd
silvester-pari <silvester.pari@eox.at>
2025-02-28 11:56:10 +0100

    fix: append matomo script to head via nuxt config

--- layouts/default.vue
+++ layouts/default.vue
@@ -229,24 +229,7 @@ export default {
     ...mapState(["appVersion"]),
   },
   mounted() {
-    setTimeout(() => {
-      var _paq = (window._paq = window._paq || []);
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push(["requireCookieConsent"]);
-      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-      _paq.push(["trackPageView"]);
-      _paq.push(["enableLinkTracking"]);
-      (function () {
-        var u = "https://nix.eox.at/piwik/";
-        _paq.push(["setTrackerUrl", u + "matomo.php"]);
-        _paq.push(["setSiteId", "13"]);
-        var d = document,
-          g = d.createElement("script"),
-          s = d.getElementsByTagName("script")[0];
-        g.async = true;
-        g.src = u + "matomo.js";
-        s.parentNode.insertBefore(g, s);
-      })();
+    this.$nextTick(() => {
       document.querySelector("esa-cookies").addEventListener("accept", () => {
         _paq.push(["rememberCookieConsentGiven"]);
       });
@@ -262,6 +245,14 @@ export default {
         document.querySelector("esa-cookies").style.display = "block";
       }
     });
+  },
+  watch: {
+    $route(to, from) {
+      _paq.push(["setCustomUrl", to.fullPath]);
+      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+      _paq.push(["trackPageView"]);
+      _paq.push(["enableLinkTracking"]);
+    },
   },
   methods: {
     catalogClick() {
